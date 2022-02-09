1cd2942a762e010b6de36a763084ae82c9d1ec59
Alb33rt <albert931023@gmail.com>
2022-02-10 01:12:24 +0800

    Navigation Bar complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 module.exports = {
     content: [
         "./src/app.html",
+        "./src/app.js",
+        "./src/theme/theme.html",
+        "./src/schedule/schedule.html",
+        "./src/schedule/schedule.js",
     ],
     theme: {
-      extend: {},
+      extend: {
+            colors: {
+                'cyan-secnd-100': "#6da79d",
+            }
+      },
+      container: {
+          center: true,
+          padding: '1em',
+      },
     },
     plugins: [],
 }
