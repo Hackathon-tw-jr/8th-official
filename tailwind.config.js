f2deb436accf5208ab3bd482386c37ff9416a2bf
Alb33rt <albert931023@gmail.com>
2022-02-10 03:49:54 +0800

    Add dist/img/index.png

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
             },
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-            }
+            },
       },
       container: {
           center: true,
